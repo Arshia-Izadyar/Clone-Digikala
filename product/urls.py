65261b06a9c2f6a5d2fced1f7497dee370955278
Arshia-Izadyar <arshiaa104@gmail.com>
2023-07-14 12:05:34 +0330

    static files work

--- product/urls.py
+++ product/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from .views import ProductHomeView
 
+app_name = 'product'
+
 urlpatterns = [
-    path('home/', ProductHomeView.as_view(), name='home'),
+    path('home/', ProductHomeView.as_view(), name='home-page'),
 ]
