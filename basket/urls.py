fc10ae46076746356d400b07f6e2ea38cfa2f3f6
Arshia-Izadyar <arshiaa104@gmail.com>
2023-07-15 17:19:30 +0330

    added delete funtion to basket

--- basket/urls.py
+++ basket/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
-from .views import ShowBasketView, AddToBasket, RemoveFromBasket
+from .views import ShowBasketView, AddToBasket, RemoveFromBasket, DeleteBasket
 
 app_name = "basket"
 
@@ -8,5 +8,7 @@ urlpatterns = [
     path("", ShowBasketView.as_view(), name="basket"),
     path("add/", AddToBasket.as_view(), name="add"),
     path("remove/", RemoveFromBasket.as_view(), name="remove"),
+    path("delete/", DeleteBasket.as_view(), name="delete"),
+    
     
 ]
