6f6de55008d9fd5c17a1e2401bb1efef028bd05e
Arshia-Izadyar <arshiaa104@gmail.com>
2023-07-15 16:20:26 +0330

    added validators

---
+++ lib/validators.py
@@ -0,0 +1,14 @@
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext as _
+
+from datetime import datetime
+
+
+def validate_rate(rate):
+    if rate < 0 or rate > 5:
+        raise ValidationError(_("Invalid rate"))
+    
+    
+def validated_date(date):
+    if date < datetime.now().date():
+        raise ValidationError(_("Inavlid Date"))
